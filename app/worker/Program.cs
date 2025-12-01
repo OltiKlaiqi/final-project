45a6451cebee2eca13dfc023685900f8a3cce3f5
OltiKlaiqi <olti.klaiqi@gmail.com>
2025-12-01 15:31:27 +0100

    semi

--- app/worker/Program.cs
+++ app/worker/Program.cs
@@ -1,12 +1,27 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Prometheus;
 
+// Start Prometheus metrics server on port 80
+var server = new MetricServer(port: 80);
+server.Start();
+
+// Create a counter for jobs processed
 var counter = Metrics.CreateCounter("worker_jobs_total", "Number of jobs processed.");
 
-while (true)
+// Increment the counter every 5 seconds in a background task
+_ = Task.Run(async () =>
 {
-    counter.Inc();
-    Console.WriteLine("Worker processed a job.");
-    Thread.Sleep(5000);
-}
+    while (true)
+    {
+        counter.Inc();
+        Console.WriteLine("Worker processed a job.");
+        await Task.Delay(5000);
+    }
+});
+
+// Keep the application alive indefinitely
+Console.WriteLine("Metrics server running on port 80. Press Ctrl+C to exit.");
+await Task.Delay(Timeout.Infinite);
+
